c776cba6852f77bac82df1742c5161dfd7a530ee
Paul Johnson <paul@snuberry-software.co.uk>
2016-12-11 23:08:42 +0000

    initial import

---
+++ relativeview/RelativePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace relativeview
+{
+    public class RelativePage : ContentPage
+    {
+        public RelativePage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
